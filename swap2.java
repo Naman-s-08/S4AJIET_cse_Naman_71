31a0d7b60f7d31443eedbb38a462f0e9ff085f0c
Naman-s-08 <madyarshrikanth@gmail.com>
2026-05-04 12:05:57 +0530

    Create swap2.java

---
+++ swap2.java
@@ -0,0 +1,13 @@
+//program to swap 2 numbers using 3rd variable
+class Main {
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+        int temp=0;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
